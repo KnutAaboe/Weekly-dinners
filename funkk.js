34b68dfc63ea2aca161fad8a9944c27516e1df2a
KnutAaboe <knutandersaaboe@gmail.com>
2021-02-16 18:55:14 +0100

    a little update

--- funkk.js
+++ funkk.js
@@ -1,5 +1,22 @@
 "use strict"
 
+// if (window.performance) {
+//   console.info("window.performance works fine on this browser");
+// }
+// console.info(performance.type);
+// if (performance.type == performance.TYPE_RELOAD) {
+//   alert("Are you sure you want to refresh page? You lose your cart when you do this")
+// } else {
+//   console.info( "This page is not reloaded");
+// }
+
+
+// $("#formname_form").submit(function(e) {
+//     e.preventDefault();
+// });
+
+
+
 if (document.readyState == "loading"){
     document.addEventListener("DOMContentLoaded", ready)
 } else {
@@ -16,13 +33,6 @@ input.addEventListener("click", function() {
     } else {
         var check = document.getElementById("tt1")
         if (check.hasChildNodes() == true) {
-
-            //FIND DINNER
-            // var list = document.getElementsByTagName("ul") 
-            // for (i = 0, i < list.length; i++;) {
-
-
-            // }
             createItem();
 
         } else {
@@ -38,24 +48,47 @@ function createDinner(){
     var node = document.createElement("ul")
     node.classList.add("t2");
     node.setAttribute("id", "dinner");
-    // node.setAttribute("onclick", "getDinner()")
     var content = document.createTextNode(dinner)
     node.appendChild(content)
     var element = document.getElementById("tt1")
     element.appendChild(node)
 
-    // element.addEventListener(click, )
-
 }
 
 function createItem(){
+
+    // var i = 0;
+    // while (check.hasChildNodes) {
+
+    // } 
+
     var item = document.getElementById("input").value
     var amount = document.getElementById("count").value
     if (amount >= 1) {
         var li = document.createElement("li")
+
+        var buttonPlus = document.createElement("button");
+        var plus = document.createTextNode("+");
+        buttonPlus.onclick = addUnit;
+        buttonPlus.appendChild(plus);
+
+        var buttonMinus = document.createElement("button");
+        var minus = document.createTextNode("-");
+        buttonMinus.onclick = removeUnit;
+        buttonMinus.appendChild(minus);
+
+        var buttonRemove = document.createElement("button");
+        var remove = document.createTextNode("remove"); //Later just red button?
+        buttonRemove.onclick = removeUnit;
+        buttonRemove.appendChild(remove);
+
         var node = document.createTextNode(item + " " + amount)
+        
         var ul = document.getElementById("dinner")
         li.appendChild(node)
+        li.appendChild(buttonMinus)
+        li.appendChild(buttonPlus)
+        li.appendChild(buttonRemove)
         ul.appendChild(li)
 
 
@@ -64,10 +97,23 @@ function createItem(){
     }
 
 
-
-   
+function addUnit() {
+    amount++;
+    // console.log(amount);
+    return amount;
 }
 
+function removeUnit() {
+    if (amount <=1) { // || this == buttonremove etC?=
+        
+    } else {
+        amount--;
+    }
+    // console.log(amount);
+    return amount;
+}
+   
+}
 
 }
 
